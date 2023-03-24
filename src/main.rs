8599964eb0490baa38ca0a8ede6dfebd725e451c
theassembler1 <lewisnoah592@gmail.com>
2023-03-23 21:28:57 -0500

    added packages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct ConnConfig {
+    port: u16,
+    ip: String,
+}
 
 #[get("/")]
 async fn check_health() -> impl Responder {
@@ -7,13 +15,17 @@ async fn check_health() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let ip = "127.0.0.1";
-    let port = 8080;
+    dotenv().ok();
 
-    println!("Server listening on http://{}:{}", ip, port);
+    let config = match envy::prefixed("SERVER_").from_env::<ConnConfig>() {
+        Ok(config) => config,
+        Err(error) => panic!("{:#?}", error),
+    };
+
+    println!("Server listening at http://{}:{}", config.ip, config.port);
 
     HttpServer::new(|| App::new().service(check_health))
-        .bind((ip, port))?
+        .bind((config.ip, config.port))?
         .run()
         .await
 }
